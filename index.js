985dffc7a166cd1f17255bbf4df6db58aa4d7c48
Saisujan15 <122602885+Saisujan15@users.noreply.github.com>
2023-02-10 16:16:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+
+var randomNumber1 = Math.floor(Math.random()*6)+1 ;
+var imageNumber = "dice"+randomNumber1+".png";
+var imageSource = "images/"+ imageNumber;
+
+var image1 = document.querySelectorAll("img")[0];
+image1.setAttribute("src" , imageSource);
+
+
+
+var randomNumber2=Math.floor(Math.random()*6)+1 ;
+var imageSource2 = "images/dice"+randomNumber2+".png";
+
+document.querySelectorAll("img")[1].setAttribute("src" , imageSource2);
+
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML="🚩Player1 wins...!";
+}
+else if(randomNumber1<randomNumber2){
+    document.querySelector("h1").innerHTML="Player 2 wins...!🚩";
+}
+else{
+    document.querySelector("h1").innerHTML="DRAW 🤪";
+}
